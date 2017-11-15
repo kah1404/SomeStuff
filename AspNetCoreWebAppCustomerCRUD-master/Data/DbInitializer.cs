f0935d27ac595a1ba2bd6e9b286199f9bb8a3e32
Kevin andre hansen <kevin-a-hansen@live.no>
2017-11-15 10:57:46 +0100

    bleh

---
+++ AspNetCoreWebAppCustomerCRUD-master/Data/DbInitializer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesContacts.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(AppDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            var cust = new Customer[]
+            {
+                new Customer {Name = "Kjell"},
+                new Customer {Name = "Olaf"},
+                new Customer {Name = "Odd"},
+                new Customer {Name = "Åsmund"},
+                new Customer {Name = "Roy"},
+
+            };
+
+            foreach (Customer s in cust)
+            {
+                context.Customers.Add(s);
+            }
+            context.SaveChanges();
+        }
+    }
+}
